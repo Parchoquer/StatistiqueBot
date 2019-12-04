43fd23240edd0d9c5eed7b1f0543042cf4d7b912
Parchoquer <bandit44000@gmail.com>
2019-12-04 03:57:31 +0100

    +dotenv +custom activity

--- StatistiqueBot.js
+++ StatistiqueBot.js
@@ -1,24 +1,27 @@
 // Made by https://github.com/Parchoquer
-// \x1b[37m\x1b[44m[+]\x1b[0m
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token, IDchannel, IDguild } = require('./config.json');
+require('dotenv').config()
 
 
-// On éteins le bot si il n'est sur aucun serveur
 client.on('ready', () => {
     if (client.guilds.size < 1) {
-        console.info(`\n\x1b[37m\x1b[44m[+]\x1b[0m StatistiqueBot n'est sur aucun serveur :( \n[+] Arrêt du script`);
+        console.info(`\n\x1b[37m\x1b[44m[+]\x1b[0m StatistiqueBot n'est sur aucun serveur :( \n\x1b[37m\x1b[44m[+]\x1b[0m Arrêt du bot !`);
         process.exit(0);
         return;
     }
 })
 
-
+const activityList = ["-info", "https://github.com/Parchoquer", "JavaScript", "Statistique"]
 client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activityList.length - 1) + 1);
+        client.user.setActivity(activityList[index]);
+    }, 5000); // 5 sec
+
     console.info(`\x1b[37m\x1b[44m[+]\x1b[0m ${client.user.tag} Tout est opérationnel!`);
-    client.user.setActivity(`Préfix: ${prefix}  |   Je compte les membres! :O`)
 });
 
 
@@ -29,9 +32,6 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
 
     if (command === 'info') {
-        // if (!args.length) {
-        //     return message.channel.send(`Vous n'avez fourni aucun argument, ${message.author}!`); // Message d'erreur SI vous n'avez pas fourni un argument dans la commande
-        // }
         message.channel.send (`:small_blue_diamond: Nom de la guilde: ${message.guild.name}\n:small_blue_diamond: Membre total: ${message.guild.memberCount}`);
     }
 });
@@ -48,5 +48,5 @@ client.on('guildMemberRemove', member => {
     client.channels.get(IDchannel).setName(`[+] Membre Total:  ${member.guild.memberCount}`);
 });
 
-
-client.login(token);
+if (process.env.TOKEN) client.login(process.env.TOKEN);
+else client.login(token);
